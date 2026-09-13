b91452d33b28829a5d1e37692fff8b234f791feb
pianzub <pianzub@gmail.com>
2026-09-13 14:08:50 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.BAKERZIN_CONFIG = {
+  APP_URL: 'https://script.google.com/macros/s/AKfycbx_XFQNvMuArwhZ-GNUJGryuRSev55euJ9gIT5MTA-pxsLfYBGskTHNfRAGy1buGqk4CA/exec',
+  VERSION: '2026.09.13-horizontal-reveal-v8'
+};
